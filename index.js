2c8f75199490bc00f686d1fdb188a29d1dba7c16
Timur Raxmanbergenov <xronicus2005gmail.com>
2025-11-26 10:58:10 +0500

    Update backend URL and frontend API calls

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const { connection } = require('mongoose');
 
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
@@ -44,7 +44,7 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true
   }
 })
